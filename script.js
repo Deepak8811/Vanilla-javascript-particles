81fd7f1ff562335ffe0abb695738dcec1185b5cb
Deepak <deepchaurasiya1997@gmail.com>
2022-07-06 10:04:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener(
+  "mousemove",
+  function (e) {
+    let body = document.querySelector("body");
+    let particles = document.createElement("span");
+    let x = e.offsetX;
+    let y = e.offsetY;
+    particles.style.left = x + "px";
+    particles.style.top = y + "px";
+    let size = Math.random() * 8;
+    particles.style.width = 2 + size+'px';
+    particles.style.height = 2 + size+'px';
+
+    body.appendChild(particles);
+    setTimeout(function () {
+      particles.remove();
+    });
+  },
+  2000
+);
